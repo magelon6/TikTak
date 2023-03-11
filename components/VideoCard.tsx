cc3e5cc908b1b566555db8a7a865e0ed28c9f350
magelon6 <adizovmsu@gmail.com>
2023-03-11 21:37:25 +0300

    user photo on each video container

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,7 +1,34 @@
-function VideoCard() {
-  return ( 
-    <div>VideoCard</div>
-   );
+import { NextPage } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { Video } from 'types'
+
+interface IProps {
+  post: Video
 }
 
-export default VideoCard;
+const VideoCard: NextPage<IProps> = ({ post }) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+          <Link href='/'>
+            <Image
+              width={62}
+              height={62}
+              className="rounded-full"
+              src={post.postedBy.image}
+              alt="profile photo"
+              layout="responsive"
+            />
+        </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import axios from "axios";
 import { PORT } from "@/globals";
 import type { Video } from '../../types';
+import NoResults from "components/NoResults";
 import VideoCard from "components/VideoCard";
 
 interface IProps {
@@ -11,10 +12,10 @@ interface IProps {
 const Home = ({ videos }: IProps) => {
   return (
       <div className="flex flex-col gap-10 videos h-full">
-        {videos.length ? (
-            videos.map(( video: Video ) => {
-                <VideoCard post={ video } key={video._id} />
-            })
+        { videos.length ? (
+           videos.map((video: Video) => (
+            <VideoCard post={video} key={video._id} />
+           )) 
         ) : (
             <NoResults text={'No Videos'}/>
         )}
@@ -25,8 +26,6 @@ const Home = ({ videos }: IProps) => {
 export async function getServerSideProps() {
 
     const { data } = await axios.get(`${PORT}/api/post`)
-
-    
 
     return {
         props: {
