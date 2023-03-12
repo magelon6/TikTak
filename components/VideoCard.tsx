76c57b19870458f9238c2bae3cdcd9d70ab0fc91
magelon6 <adizovmsu@gmail.com>
2023-03-12 15:00:42 +0300

    video container

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Video } from 'types'
+import { GoVerified } from 'react-icons/go';
 
 interface IProps {
   post: Video
@@ -10,10 +11,10 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div className='flex-col'>
+    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
       <div>
-        <div className="flex align-middle items-center">
-          <div className="w-12 m-2">
+        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded '>
+          <div className='md:w-16 md:h-16 w-10 h-10'>
             <Link href='/'>
               <Image
                 width={62}
@@ -27,22 +28,40 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           </div>
           <div className="text-lg ">
             <Link href='/'>
-              <p>{post.postedBy.userName} </p>
+            <div className='flex items-center gap-2'>
+                <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                  {post.postedBy.userName}{' '}
+                  <GoVerified className='text-blue-400 text-md' />
+                </p>
+                <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                  {post.postedBy.userName}
+                </p>
+              </div>
             </Link>
           </div>
         </div>
       </div>
-
-      <div className="flex relative">
-        <div className="absolute inset-0">
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div className="rounded-3xl">
           <Link href="/">
-            <video className=" object-cover bg-gray-600" loop muted>
-              <source src={post.video.asset.url} type="video/mp4" />
+            <video 
+              className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100' 
+              poster={post.video.asset.url} 
+              loop 
+              muted
+            >
               Your browser does not support the video tag.
             </video>
           </Link>
         </div>
       </div>
+
+        {/* <div className="relative w-full h-0" style={{ paddingBottom: "56.25%" }}>
+          <div className="max-h-100">
+
+          <img src={post.video.asset.url} className="absolute inset-0 w-100 h-100 object-cover" />
+          </div>
+        </div> */}
     </div>
   )
 }
