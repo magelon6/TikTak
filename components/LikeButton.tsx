7494d0ea55da5e39ce2f9300276dadafe82d2563
magelon6 <adizovmsu@gmail.com>
2023-03-19 17:59:47 +0300

    feat: like functionality

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdFavorite } from 'react-icons/md'
 
 import useAuthStore from '../state/authStore'
 
-const LikeButton = () => {
+interface IProps {
+  handleLike: () => void,
+  handleDislike: () => void,
+  likes: any[]
+}
 
-  const [liked, setLiked] = useState(true);
-  const { userProfile } = useAuthStore();
+const LikeButton = ({ likes, handleLike, handleDislike }: IProps) => {
+
+  const [liked, setLiked] = useState(false);
+  const { userProfile }: any = useAuthStore();
+  const filterLikes = likes?.filter((item) => item._ref === userProfile?._id);
+
+  useEffect(() => {
+    if(filterLikes?.length > 0){
+      setLiked(true)
+    }else{
+      setLiked(false)
+    }
+  }, [likes, filterLikes])
+  
 
   return (
-    <div className="gap-6">
+    <div className="flex gap-6">
       <div className="mt-4 flex flex-col justify-center items-center cursor-pointer">
         {liked ? (
           <div className="bg-primary rounded-full p-2 md:p-4   text-[#f51919]" onClick={handleDislike}>
@@ -20,7 +36,7 @@ const LikeButton = () => {
             <MdFavorite className="text-lg md:text-2xl" />
           </div>
         )}
-
+        <p className="text-md font-semibold">{likes?.length || 0}</p>
       </div>
     </div>
   )
